f0931a10792b8de3e8dbb274c0debfd8955b1de4
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2016-01-10 06:35:13 +0900

    天気予報ツイート

--- weather_bot.php
+++ weather_bot.php
@@ -14,7 +14,7 @@ $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $acc
 
 
 
-/* 天気予報 */
+/* 天気予報 （東京）*/
 $url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010";
 $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
