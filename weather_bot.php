df68788f86d8479131155a2129b2bffcb7a5f095
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2016-01-10 05:46:41 +0900

    api include

---
+++ weather_bot.php
@@ -0,0 +1,14 @@
+<?php 
+// twitterOAuth の読み込み
+require_once 'autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+require_once('config.php');
+
+// OAuth認証
+$consumerKey = API_KEY;
+$consumerSecret = API_SECRET;
+$accessToken = ACCESS_TOKEN;
+$accessTokenSecret = ACCESS_SECRET;
+$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+$status = $connection->post('statuses/update', ['status' => $text]);
